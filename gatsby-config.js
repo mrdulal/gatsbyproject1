4a744aaeb3f099837227989e787de5eab5cc8d77
sanjaya dulal <wwwdevwp@gmail.com>
2020-02-12 09:10:30 +0545

    gatsby workup

---
+++ gatsby-config.js
@@ -0,0 +1,78 @@
+// In your gatsby-config.js
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from WordPress.
+     */
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'demo.wp-api.org' or 'www.example-site.com'
+         */
+        baseUrl: "koshionline.com",
+        // The protocol. This can be http or https.
+        protocol: "https",
+        // The rest api route prefix that your WordPress site is using.
+        // Sometimes this is modified by WordPress plugins.
+        // If not set, it uses the default of "wp-json"
+        restApiRoutePrefix: "wp-json",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on wordpress.com.
+        // Defaults to true.
+        useACF: false,
+        // Include specific ACF Option Pages that have a set post ID
+        // Regardless if an ID is set, the default options route will still be retrieved
+        // Must be using V3 of ACF to REST to include these routes
+        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
+        // routes with the ID option_page_1 and option_page_2
+        // The IDs provided to this array should correspond to the `post_id` value when defining your
+        // options page using the provided `acf_add_options_page` method, in your WordPress setup
+        // Dashes in IDs will be converted to underscores for use in GraphQL
+        acfOptionPageIds: [],
+
+        // Set how many pages are retrieved per API request.
+        perPage: 100,
+
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: ["**/posts/1456"],
+        // Set this to keep media sizes.
+        // This option is particularly useful in case you need access to
+        // URLs for thumbnails, or any other media detail.
+        // Defaults to false
+        keepMediaSizes: false,
+
+      },
+    },
+  ],
+}
